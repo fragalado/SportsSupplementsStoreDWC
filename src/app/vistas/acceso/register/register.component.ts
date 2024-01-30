75532c7b0df030afbbbb9709b52e9b67d79453c7
DESKTOP-V1IS21O\Puesto8 <frandanigd@gmail.com>
2024-01-30 08:07:46 +0100

    29/01/2024, cambios

--- src/app/vistas/acceso/register/register.component.ts
+++ src/app/vistas/acceso/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Usuario } from 'src/app/modelos/usuario';
 import { DatabaseService } from 'src/app/servicios/database.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -21,16 +22,38 @@ export class RegisterComponent {
 
   constructor(private formBuilder: FormBuilder, private dbs: DatabaseService, private router: Router){}
 
-  registrarUsuario(){
+  registrarUsuario() {
     // Actualizamos el usuario con los datos del formulario
     this.usuario.nombre = this.formRegistro.controls['nombre'].value!;
     this.usuario.email = this.formRegistro.controls['email'].value!;
     this.usuario.telefono = this.formRegistro.controls['telefono'].value!;
     this.usuario.password = this.formRegistro.controls['password'].value!;
 
-    // Hacemos un insert del usuario a la base de datos
+    // Comprobamos que el email introducido no exista ya un usuario con el email
+    var ok = true;
+    this.dbs.queryCollection('usuarios', 'email', this.usuario.email).subscribe(res => {
+      if (res.length > 1) {
+        console.log(res);
+        console.log("Hay usuario ya creado");
+        ok = false;
+      }
+    })
+
+    if(ok){
+      // Hacemos un insert del usuario a la base de datos
     this.dbs.newDocument(this.usuario, 'usuarios')
-      .then()
-      .catch();
+    .then(() => Swal.fire({
+      title: "Creado",
+      text: "Creado con éxito!!",
+      icon: "success"
+    }))
+    .catch(() => Swal.fire({
+      title: "Oops..",
+      text: "Se ha producido un error. Vuelva a intentarlo más tarde.",
+      icon: "error"
+    }));
+    }
+
+
   }
 }
